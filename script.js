432c4125aa4631d1ab711cbc4c5f28b8bb9560c8
jamieYeung <jamieyyw@gmail.com>
2019-09-04 18:50:38 -0400

    filter finally works, no duplicates and if duplicate is tried to added, then console error you have bought this movie

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// alert("hello world"); script is liked
-
-
-
 // 4 Options the user can select
-
-
 //1. Show all movies from json file that the user can select from available_movies, sorted alphabetically and by genre showing title/rating/cost
 
 // empty array to push list of movies inside
@@ -115,10 +109,10 @@ let strHor = '';
 let strRom = '';
 let strSci = '';
 let strThr = '';
-// users movie list
+// puts users movie list on page when my movies is selected
 let strUsr = '';
 
-// async function for movies
+// async function for getting movies from json
 async function getData() {
   const response = await fetch(url);
   const data = await response.json()
@@ -135,7 +129,6 @@ function getAction(){
       const second = removeArticles(b.title.toUpperCase());
       return (first < second) ? -1 : (first > second) ? 1 : 0;
     })
-    console.log(sorted);
     sorted.forEach(movie => {
       strAct += `
       <ul class=${'movies'}>
@@ -158,13 +151,12 @@ function getComedy() {
         const second = removeArticles(b.title.toUpperCase());
         return (first < second) ? -1 : (first > second) ? 1 : 0;
       })
-      // console.log(sorted);
       sorted.forEach(movie => {
         strCom += `
       <ul class=${'movies'}>
         <li> Title: ${movie.title}</li> 
-        <li> Cost: ${movie.cost}</li> 
         <li> Rating: ${movie.rating}</li>
+        <li> Cost: ${movie.cost}</li> 
       </ul>  
       `
       });
@@ -181,13 +173,12 @@ function getDocumentary() {
         const second = removeArticles(b.title.toUpperCase());
         return (first < second) ? -1 : (first > second) ? 1 : 0;
       })
-      // console.log(sorted);
       sorted.forEach(movie => {
         strDoc += `
       <ul class=${'movies'}>
         <li> Title: ${movie.title}</li> 
-        <li> Cost: ${movie.cost}</li> 
         <li> Rating: ${movie.rating}</li>
+        <li> Cost: ${movie.cost}</li> 
       </ul>  
       `
       });
@@ -204,13 +195,12 @@ function getDrama() {
         const second = removeArticles(b.title.toUpperCase());
         return (first < second) ? -1 : (first > second) ? 1 : 0;
       })
-      // console.log(sorted);
       sorted.forEach(movie => {
         strDra += `
       <ul class=${'movies'}>
         <li> Title: ${movie.title}</li> 
-        <li> Cost: ${movie.cost}</li> 
         <li> Rating: ${movie.rating}</li>
+        <li> Cost: ${movie.cost}</li> 
       </ul>  
       `
       });
@@ -227,13 +217,12 @@ function getHorror() {
         const second = removeArticles(b.title.toUpperCase());
         return (first < second) ? -1 : (first > second) ? 1 : 0;
       })
-      // console.log(sorted);
       sorted.forEach(movie => {
         strHor += `
       <ul class=${'movies'}>
         <li> Title: ${movie.title}</li> 
-        <li> Cost: ${movie.cost}</li> 
         <li> Rating: ${movie.rating}</li>
+        <li> Cost: ${movie.cost}</li> 
       </ul>  
       `
       });
@@ -250,13 +239,12 @@ function getRomance() {
         const second = removeArticles(b.title.toUpperCase());
         return (first < second) ? -1 : (first > second) ? 1 : 0;
       })
-      // console.log(sorted);
       sorted.forEach(movie => {
         strRom += `
       <ul class=${'movies'}>
         <li> Title: ${movie.title}</li> 
-        <li> Cost: ${movie.cost}</li> 
         <li> Rating: ${movie.rating}</li>
+        <li> Cost: ${movie.cost}</li> 
       </ul>  
       `
       });
@@ -273,13 +261,12 @@ function getScifi() {
         const second = removeArticles(b.title.toUpperCase());
         return (first < second) ? -1 : (first > second) ? 1 : 0;
       })
-      // console.log(sorted);
       sorted.forEach(movie => {
         strSci += `
       <ul class=${'movies'}>
         <li> Title: ${movie.title}</li> 
-        <li> Cost: ${movie.cost}</li> 
         <li> Rating: ${movie.rating}</li>
+        <li> Cost: ${movie.cost}</li> 
       </ul>  
       `
       });
@@ -296,13 +283,12 @@ function getThriller() {
         const second = removeArticles(b.title.toUpperCase());
         return (first < second) ? -1 : (first > second) ? 1 : 0;
       })
-      // console.log(sorted);
       sorted.forEach(movie => {
       strThr += `
       <ul class=${'movies'}>
         <li> Title: ${movie.title}</li> 
-        <li> Cost: ${movie.cost}</li> 
         <li> Rating: ${movie.rating}</li>
+        <li> Cost: ${movie.cost}</li> 
       </ul>  
       `
       });
@@ -314,36 +300,14 @@ function getThriller() {
 //2. Show all movies that the user has selected from my_movies.json
 const urlUserMovies = 'my_movies.json'
 const userMovies = []
-
-// empty array to put data of my_movies in
-// can put on page
-// add if watched or not
-
 async function getMyMovies() {
   const response = await fetch(urlUserMovies);
   const data = await response.json()
   return data;
 }
 function getUserMovies(){
-  getMyMovies()
-    .then(data => {
-      userMovies.push(data);
-      // action movies
-      // sortedAction = userMovies[0].action;
-      // console.log(sortedAction);
-      // sortedAction.forEach(movie => {
-      //   strUsr += `
-      // <ul class=${'movies'}>
-      //   <li> Title: ${movie.title}</li> 
-      //   <li> Cost: ${movie.cost}</li> 
-      //   <li> Rating: ${movie.rating}</li>
-      //   <li> Watched: ${movie.watched}</li>
-      // </ul>  
-      // `
-      // });
-      sorted = userMovies[0].scifi;
-      // console.log(sorted);
-      sorted.forEach(movie => {
+
+      filteredMovies.forEach(movie => {
       strUsr += `
       <ul class=${'movies'}>
         <li> Title: ${movie.title}</li> 
@@ -354,46 +318,53 @@ function getUserMovies(){
       `
       });
       document.querySelector(".userList").innerHTML = strUsr;
-    });
+    // });
 }
-
-
 //3. User is shown their credit balance and returned to the selection menu. Initial is 100 credits
 
 // user credit balance 
-let userCreditBalance = 100;
+// should be 100, 10 for test
+let userCreditBalance = 10;
 const btnShowCreditBal = document.getElementById('userCreditBalance');
 const creditBalance = document.querySelector('.creditBalance')
 btnShowCreditBal.addEventListener('click', showBalance);
 btnShowCreditBal.addEventListener('click', clearPage)
 
-
+// clears the page and only shows user's balance
 function clearPage(){
   movieList.style.display = 'none';
   userList.style.display = 'none';
   btnAll.style.display = 'block';
   btnUser.style.display = 'block';
 }
-
-
-
+// shows users credit balance
 function showBalance(){
+  console.log(filteredMovies);
+  // showCost();
+  let total = filteredMovies.reduce((acc, cur) => {
+    return acc + cur.cost;
+  }, 0)
+   console.log(total);
+   if(total > 10){
+     console.log("not enough credit")
+   }else{
   creditBalance.innerHTML = `Your Current Credit Balance: 
-    ${userCreditBalance}`; 
+    ${userCreditBalance - total}`;
+   } 
 }
-
-
 //4. 
 // User can purchase a movie by typing the name of the movie. If movie is owned alredy, the string "You have already purchased this movie" should display and user is returned to select.
-// need to add purchase button to each movie
-// Each purchase deducts from the user's credit based on cost of the movie
-// If not enough credits display the string "Not enough credit remaining to purchase this movie"
-// when movie is purchased, it is added to my_movies.json 
 
-// purchase movie button has to do :
+// Each purchase deducts from the user's credit based on cost of the movie -- done
+// If not enough credits display the string "Not enough credit remaining to purchase this movie" -- need to work on this
+// when movie is purchased, it is added to my_movies.json  -- adding to array instead
+
+// purchase/submit purchase
 // 1. When typed in, adds the movie to my_movies.json ---> add to array
-// 2. If movie is owned already, display "You have already purchased this movie" and returned to selection
-// 3. Each purchase deducts from the credit balance, if not enough credit remaining to purchase display "Not enough credit remaining to purchase this"
+
+// 2. If movie is owned already, display "You have already purchased this movie" and returned to selection -- need to do this
+
+// 3. Each purchase deducts from the credit balance, if not enough credit remaining to purchase display "Not enough credit remaining to purchase this"--- can deduct but need condition of no credit left and do not add
 
 btnPurchase = document.getElementById("purchaseMovie")
 // btnPurchase.addEventListener('click', purchaseMovie);
@@ -404,19 +375,7 @@ const form = document.getElementById('form');
 // compares movies to the input of the user and matches
 // adds all movies that the user has typed in
 let purchasedMovies = [];
-// let filteredMovies = [];
-
-// function removeDups(purchasedMovies) {
-//   let unique = {};
-//   purchasedMovies.forEach(function (i) {
-//     if (!unique[i]) {
-//       unique[i] = true;
-//       console.log(unique[i])
-//     }
-//   });
-//   return Object.keys(unique);
-// }
-
+let filteredMovies = [];
 form.addEventListener('submit',function(e){
   e.preventDefault();
   // for matching to movie titles
@@ -424,52 +383,59 @@ form.addEventListener('submit',function(e){
   // calls data here
   getData().then( movies =>{
     // console.log(movies);
-      for(let key in movies){
-        // console.log([key]);
-        let moviesOne = movies[key]
-        console.log(moviesOne)
-        for(let i = 0; i< moviesOne.length; i++){
-          // console.log(moviesOne[i].title)
-          // matches input of the user to title of the movie
-            if (movieInput === moviesOne[i].title.toUpperCase() ) {
-              console.log("found movie!")
-              // pushes the input of the user to the purchased movies array if typed in correctly
-              console.log(purchasedMovies.push(moviesOne[i]))
-              // creates a new array that will filter out any duplicate entries from te input
-                filteredArr = purchasedMovies.reduce((acc, cur) => {
-                let movieName = acc.find(movie => movie.title === cur.title);
-                // if the titles do not match, add the movie to the filtered arr
-                if (!movieName) {
-                  // console.log(cur);
-                  console.log(movieName);
-                  return acc.concat([cur]);
-                  // if they do match, do not add the movie and alert that the movie is already owned
-                } else {
-                  console.log(movieName);
-                  alert('You own this movie');
-                  // console.log(acc);
-                  return acc;
-                }
-                
-              }, [])
-              console.log(filteredArr);
-              return;
+    for(let key in movies){
+      // console.log([key]);
+      let moviesOne = movies[key]
+      // console.log(moviesOne)
+      for(let i = 0; i< moviesOne.length; i++){
+        // console.log(moviesOne[i])
+        // console.log(moviesOne[i].cost)
+        // matches input of the user to title of the movie
+          if (movieInput === moviesOne[i].title.toUpperCase() ) {
+            console.log("found movie!")
+            // pushes the input of the user to the purchased movies array if typed in correctly
+            filteredMovies.push(moviesOne[i])
+            // creates a new array that will filter out any duplicate entries from filteredMovies input
+            filteredMovies = filteredMovies.reduce((acc, cur) => {
+            let movieName = acc.find(movie => movie.title === cur.title);
+            // let movieDup = [cur].find(movie => movie.title === cur.title)
+            // if the titles do not match, add the movie to the filteredMovies
+            if (!movieName ) {
+              // console.log(cur);
+              // console.log(acc.concat([cur]));
+              return acc.concat([cur]);
+              // if they do match, do not add the movie to filteredMovies and alert that the movie is already owned
+            } else{
+              // this is the title of the movie to compare to
+              // console.log(movieInput);
+              // console.log(movieDup);
+              console.error(`You have already purchased ${movieName.title}`);
+              // console.log(acc);
+              return acc;
             }
+          }, [])
+          // console.log(purchasedMovies);
+          console.log(filteredMovies);
+          return;
           }
+        }
       }
-
-      // return;
-    // removeDups(purchasedMovies);
     console.log("not found...")
   })
   .catch(err => console.log(err))
   form.reset();
-  console.log(purchasedMovies);
-    // console.log(filteredMovies);
 })
 
 // on submit of input, input value is matched to all movies
 // if matched 
+
+// problems ******
+
+// when submitted, movie is pushed in to purchased movies
+// when purchased movies is filtered, if statement is comparing to pruchased movies, so alert pops up everytime there is a duplicate in purchased movies --- need to fix
+
+// purchased movies is filtered to filtered movies
+// need fo fix way when filtered movies to not push if total cost of movies is > remaining credit balance
 
 
 
@@ -500,3 +466,22 @@ form.addEventListener('submit',function(e){
 //     return movieTitles;
 //   })
 // }
+
+// getMyMovies()
+//   .then(data => {
+//     userMovies.push(data);
+// action movies
+// sortedAction = userMovies[0].action;
+// console.log(sortedAction);
+// sortedAction.forEach(movie => {
+//   strUsr += `
+// <ul class=${'movies'}>
+//   <li> Title: ${movie.title}</li> 
+//   <li> Cost: ${movie.cost}</li> 
+//   <li> Rating: ${movie.rating}</li>
+//   <li> Watched: ${movie.watched}</li>
+// </ul>  
+// `
+// });
+// sorted = userMovies[0].scifi;
+// console.log(sorted);
